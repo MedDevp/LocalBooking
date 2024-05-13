e5097dcb1d07bb5ca460b94a9697130df91a49ef
MedDev <157510275+MedDevp@users.noreply.github.com>
2024-05-13 13:49:46 +0100

    add vite + react

--- BackendSalles/src/main/java/org/example/backendsalles/Controller/EventController.java
+++ BackendSalles/src/main/java/org/example/backendsalles/Controller/EventController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173/")
 @RequestMapping("/api/events")
 public class EventController {
     private final EventService eventService;
@@ -22,5 +22,6 @@ public class EventController {
     public ResponseEntity<Event> createEvent(@RequestBody Event event) {
         Event savedEvent = eventService.saveEvent(event);
         return new ResponseEntity<>(savedEvent, HttpStatus.CREATED);
+
     }
 }
